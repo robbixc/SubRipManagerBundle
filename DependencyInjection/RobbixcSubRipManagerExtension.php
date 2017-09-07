a2c6f7659720a43078e6aa333bc0223f7bc7f5ba
Roberto Cucinotta <roberto.cucinotta@trivago.com>
2017-09-07 22:49:58 +0200

    fix missing use statement

--- DependencyInjection/RobbixcSubRipManagerExtension.php
+++ DependencyInjection/RobbixcSubRipManagerExtension.php
@@ -4,6 +4,7 @@ namespace Robbixc\SubRipManagerBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
